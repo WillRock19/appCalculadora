b0a3fba4f35b00a53a4e0f850a51aa660383de57
William Pinto <william.pinto@lambda3.com.br>
2019-12-22 12:41:55 -0300

    Created Display component and added it to the App.js

--- App.js
+++ App.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
 import Button from './src/components/Button';
+import Display from './src/components/Display';
 import { View, StyleSheet } from 'react-native';
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      displayValue: '0',
+    };
+  }
+
   render() {
     return (
       <View>
         <View style={styles.container}>
+          <Display value={this.state.displayValue} />
           <View style={styles.buttons}>
             <Button label="AC" />
+            <Button label="/" />
+            <Button label="7" />
+            <Button label="8" />
+            <Button label="9" />
+            <Button label="*" />
+            <Button label="4" />
+            <Button label="5" />
+            <Button label="6" />
+            <Button label="-" />
+            <Button label="1" />
+            <Button label="2" />
+            <Button label="3" />
+            <Button label="+" />
+            <Button label="0" />
+            <Button label="." />
+            <Button label="=" />
           </View>
         </View>
       </View>
@@ -19,8 +44,9 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
+  buttons:{
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  }
 });
---
+++ src/components/Display/Display.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Styles from './Style';
+import { View, Text } from 'react-native';
+
+export default props => (
+  <View style={Styles.display}>
+    <Text style={Styles.displayValue} numberOfLines={1} ellipsizeMode={'head'}>
+      {props.value}
+    </Text>
+  </View>
+);
