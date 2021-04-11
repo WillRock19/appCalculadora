2d72d923b0baf1bb11a90f70397e4613c7436d6b
William <william.pinto@lambda3.com.br>
2021-04-11 11:36:48 -0300

    TEsts with automatic tests

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,21 +1,25 @@
 import 'react-native';
 import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
-import Enzyme, { mount } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import App from '../App';
+import renderer from 'react-test-renderer';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 let calculator;
 
 describe('Calculator App', () => {
-  beforeEach(() => {
-    calculator = mount(<App />);
-  });
+  // calculator = renderer.create(<App />);
+  calculator = shallow(<App />);
 
   describe('on initialization', () => {
     it('should show 0 value in the display', () => {
-      console.log(calculator.find('Text[testId="display-value"]'));
+      console.log('Valor: ', calculator);
+      console.log('A root: ', calculator.instance());
+      // console.log(calculator.root.find('Text[testId="display-value"]'));
+
+      expect(true).toBe(true);
     });
   });
 });
